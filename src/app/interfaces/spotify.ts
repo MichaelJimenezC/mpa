af754917d46d004c741f49528cdeabace81cde46
Michael Jiménez <micjejim@espol.edu.ec>
2023-12-04 10:55:31 -0500

    ERROR

--- src/app/interfaces/spotify.ts
+++ src/app/interfaces/spotify.ts
@@ -1,2 +1,26 @@
 export interface Spotify {
-}
+    "acousticness_%": string
+    "artist(s)_name": string
+    artist_count: string
+    bpm: string
+    "danceability_%": string
+    "energy_%": string
+    in_apple_charts: string
+    in_apple_playlists: string
+    in_deezer_charts: string
+    in_deezer_playlists: string
+    in_shazam_charts: string
+    in_spotify_charts: string
+    in_spotify_playlists: string
+    "instrumentalness_%": string
+    key: string
+    "liveness_%": string
+    mode: string
+    released_day: string
+    released_month: string
+    released_year: string
+    "speechiness_%": string
+    streams: string
+    track_name: string
+    "valence_%": string
+  }
