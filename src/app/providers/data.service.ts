4628dfa51bca41d1a5ae1f8c582f0dec0678fcaa
MichaelJimenezC <micjejim@espol.edu.ec>
2023-12-20 09:24:16 -0500

    arreglado datos

--- src/app/providers/data.service.ts
+++ src/app/providers/data.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
-  private URL: string = 'https://mpadawm.firebaseio.com/collection.json';
+  private URL: string = 'https://mpadawm-default-rtdb.firebaseio.com/collection.json';
 
   constructor(private http:HttpClient) { 
 
